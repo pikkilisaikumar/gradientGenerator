c7568a07e3bf77b18b911e04e7820c7a0ac22fe2
pikkilisaikumar <pikkilisaikumar@gmail.com>
2022-01-17 07:53:36 +0000

    doubt

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const Button = styled.button`
   border-width: 0px;
   background-color: ${props => props.bgColor};
-  color: white;
   opacity: ${props => props.opacity};
 `
 export default Button
